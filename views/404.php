04f56a05f8594653173074bb7ead52ba6a2772db
Grimaldi Baptiste <79135575+GrimalDev@users.noreply.github.com>
2023-05-08 19:19:41 +0200

    styling changes 404

--- views/404.php
+++ views/404.php
@@ -1,2 +1,23 @@
-<link rel="shortcut icon" type="image/jpg" href="../style/media/TaBeRu-solid-fit.png"/>
-<h1>404</h1>
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" type="image/jpg" href="../style/media/TaBeRu-solid-fit.png"/>
+
+    <title>Not Found</title>
+
+    <script defer src="../scripts/script-general.js" type="application/javascript"></script>
+</head>
+<body>
+    <!--Get header template-->
+    <?php include realpath(dirname(__FILE__) . '/partials/header.php')?>
+
+    <h1>Page Not Found</h1>
+
+    <!--Get footer template-->
+    <?php include realpath(dirname(__FILE__) . '/partials/footer.html')?>
+</body>
+</html>
