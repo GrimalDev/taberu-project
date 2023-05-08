0b47659c6b7a5cc27c359288db699807ae9758a8
Grimaldi Baptiste <79135575+GrimalDev@users.noreply.github.com>
2023-05-08 18:56:47 +0200

    delete out of project image

--- app/redirection.php
+++
@@ -1,8 +0,0 @@
-<?php
-function redirect($url) {
-    ob_start();
-    //ob_start() démarre la temporisation de sortie. Tant qu'elle est enclenchée, aucune donnée, hormis les en-têtes, n'est envoyée au navigateur, mais temporairement mise en tampon.
-    header('Location: '.$url);
-    ob_end_flush();
-    die();
-}
--- views/support.php
+++
@@ -1,2 +0,0 @@
-<link rel="shortcut icon" type="image/jpg" href="../style/media/TaBeRu-solid-fit.png"/>
-<h1>Support</h1>
