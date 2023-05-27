950b5126589680850fc1456a7faf5006f7dd8066
Grimaldi Baptiste <79135575+GrimalDev@users.noreply.github.com>
2023-05-27 19:47:58 +0200

    faster loading screen

--- scripts/script-general.js
+++ scripts/script-general.js
@@ -1,19 +1,24 @@
+const main = document.querySelector('main')
 const burgerMenu = document.getElementById('animated-burger')
 const pulse = document.getElementById('relative-circle-animation-container')
-const allCardsContainer = document.getElementById('all-generated-cards-container')
+const allCardsContainer = document.getElementsByClassName('all-generated-cards-container')[0];
 
 burgerMenu.addEventListener("click", function () {
-    burgerMenu.classList.toggle("burger-active")
+  burgerMenu.classList.toggle("burger-active")
 });
 
-async function removePulse() {
-    const delay = ms => new Promise(res => setTimeout(res, ms));
-    await delay(3500);
-    pulse.classList.toggle("make-disappear");
-    allCardsContainer.classList.toggle("make-disappear");
+async function pulseAnimationHandler() {
+  const delay = ms => new Promise(res => setTimeout(res, ms));
+  await delay(2500);
+  pulse.classList.toggle("make-disappear");
+  allCardsContainer.classList.toggle("make-disappear");
 }
 
-removePulse()
+const init = async () => {
+  await pulseAnimationHandler();
+}
+
+window.onload = init;
 
 /*-------------Gestion des cartes de recettes-------------*/
 
