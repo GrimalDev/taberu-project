e92c15cf546711d5b02090b7d6e5563e1cf839c4
Grimaldi Baptiste <79135575+GrimalDev@users.noreply.github.com>
2023-05-29 21:48:17 +0200

    register bug fix
    user class implementation

--- app/controllers/user.php
+++ app/controllers/user.php
@@ -102,6 +102,68 @@ class user
         return true;
     }
 
+    public function createUser() : bool|Exception
+    {
+        $DBpdo = connectDB();
+        $DBtablename = 'users';
+
+        try {
+            $query = $DBpdo->prepare("INSERT INTO `$DBtablename` (`username`, `email`, `password`, `role`) VALUES (:username, :email, :password, :role)");
+            $query->bindParam(':username', $this->username, PDO::PARAM_STR);
+            $query->bindParam(':email', $this->email, PDO::PARAM_STR);
+            $query->bindParam(':password', $this->passHash, PDO::PARAM_STR);
+            $query->bindParam(':role', $this->role, PDO::PARAM_STR);
+            $query->execute();
+        } catch (PDOException $e) {
+            if ($e->errorInfo[1] == 1062) {
+                return new Exception("Un compte est déja associé à cette email");
+            }
+            return new Exception("Erreur lors de l'ajout de l'utilisateur");
+        }
+        return true;
+    }
+
+    public function updateUser() : bool|Exception
+    {
+        $DBpdo = connectDB();
+        $DBtablename = 'users';
+
+        //only modify fields that are set
+        $query = "UPDATE `$DBtablename` SET ";
+        $query .= ($this->username !== null) ? "`username` = :username, " : "";
+        $query .= ($this->email !== null) ? "`email` = :email, " : "";
+        $query .= ($this->passHash !== null) ? "`password` = :password, " : "";
+        $query .= ($this->role !== null) ? "`role` = :role, " : "";
+        $query = substr($query, 0, -2);
+        $query .= " WHERE `id` = :id";
+
+        $query = $DBpdo->prepare($query);
+        $query->bindParam(':id', $this->id, PDO::PARAM_INT);
+        if ($this->username !== null) {
+            $query->bindParam(':username', $this->username, PDO::PARAM_STR);
+        }
+        if ($this->email !== null) {
+            $query->bindParam(':email', $this->email, PDO::PARAM_STR);
+        }
+        if ($this->passHash !== null) {
+            $query->bindParam(':password', $this->passHash, PDO::PARAM_STR);
+        }
+        if ($this->role !== null) {
+            $query->bindParam(':role', $this->role, PDO::PARAM_STR);
+        }
+
+        try {
+            $query->execute();
+        } catch (PDOException $e) {
+            if ($e->errorInfo[1] == 1062) {
+                return new Exception("Un compte est déja associé à cette email");
+            }
+            return new Exception("Erreur lors de la modification de l'utilisateur");
+        }
+
+        return true;
+    }
+
     public function verifyPassword($password): bool
     {
         //verify passsword hash is set
@@ -175,6 +237,11 @@ class user
         $this->passHash = $passHash;
     }
 
+    public function setPassHashFromPassword($password): void
+    {
+        $this->passHash = self::hashPassword($password);
+    }
+
     /**
      * @return mixed
      */
@@ -191,7 +258,12 @@ class user
         $this->role = $role;
     }
 
-
+    public static function hashPassword($password) {
+    $options = [
+        'cost' => 9 // A value used by the hashing algorithm
+    ];
+    return password_hash($password, PASSWORD_DEFAULT, $options);
+}
 
     public static function logConnection($user_id): void
     {
--- views/register.php
+++ views/register.php
@@ -2,6 +2,7 @@
 
 require_once(realpath(dirname(__FILE__) . '/../app/db-config.php'));
 require_once(realpath(dirname(__FILE__) . '/../app/redirection.php'));
+require_once(realpath(dirname(__FILE__) . '/../app/controllers/user.php'));
 
 //verify if user is logged in
 session_start();
@@ -16,7 +17,7 @@ $DBpdo = connectDB();
 
 function isFormValidated() { // check for validated form, get variables, and execute the validations
 
-    if (!isset($_POST["submit"])) { return; };
+    if (!isset($_POST["submit"])) { return ""; };
 
     if (!isset($_POST["username"]) && !isset($_POST["email"]) && !isset($_POST["password"])) { return "Veuillez renseigner tout les champs"; };
 
@@ -44,30 +45,17 @@ function verifyFormData() {
     return sendDataDB(); // do stuff with the data
 }
 
-function hashPassword($password) {
-    $options = [
-        'cost' => 9 // A value used by the hashing algorithm
-    ];
-    return password_hash($password, PASSWORD_DEFAULT, $options);
-}
-
 function sendDataDB() { // process the data coming from the form
-    $DBpdo = connectDB();
-    $DBtablename = 'users';
-    $username = FORM_USERNAME;
-    $email = FORM_EMAIL;
-    $hashed_password = hashPassword(FORM_PASSWORD);
 
     try {
-        $query = $DBpdo->prepare("INSERT INTO `$DBtablename` (`username`, `email`, `password`) VALUES (:username, :email, :password)");
-        $query->bindParam(':username', $username);
-        $query->bindParam(':email', $email); // default PDO::PARAM_STR
-        $query->bindParam(':password', $hashed_password); // default PDO::PARAM_STR
-        $query->execute();
+        $user = new user();
+        $user->setUsername(FORM_USERNAME);
+        $user->setEmail(FORM_EMAIL);
+        $user->setRole('regular');
+        $user->setPassHashFromPassword(FORM_PASSWORD);
+        $user->createUser();
     } catch (PDOException $e) {
-        if ($e->errorInfo[1] == 1062) {
-            return "Un compte est déja associé à cette email";
-        }
+        return $e;
     }
     redirect(' /connection');
 }
@@ -97,8 +85,8 @@ $FORM_ERRORS = isFormValidated();
     <main>
         <form action="/enregistrement" method="post">  <!--TODO implement form animation-->
             <div><pre><?php echo $FORM_ERRORS; ?></pre></div>
-            <input type="username" name="username" placeholder="Entrez votre nom d'utilisateur" required>
-            <input type="email" name="email" placeholder="Entrez votre e-mail" required>
+            <input type="username" name="username" placeholder="Entrez votre nom d'utilisateur" <?php echo isset($_POST["username"]) ? 'value="' .$_POST["username"] . '"' : ''  ?> required>
+            <input type="email" name="email" placeholder="Entrez votre e-mail" <?php echo isset($_POST["email"]) ? 'value="' . $_POST["email"] . '"' : ''  ?> required>
             <input type="password" name="password" placeholder="Entrez votre mot de passe" required>
             <input type="password" name="password-confirm" placeholder="Confirmer votre mot de passe" required> <!--TODO add view password button-->
             <input type="submit" name="submit" value="S'inscrire">
