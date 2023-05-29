9b9aab5d84e66045bfbdabcd262872081e284e92
Grimaldi Baptiste <79135575+GrimalDev@users.noreply.github.com>
2023-05-29 12:26:44 +0200

    new animations

--- scripts/script-general.js
+++ scripts/script-general.js
@@ -1,24 +1,5 @@
-const main = document.querySelector('main')
 const burgerMenu = document.getElementById('animated-burger')
-const pulse = document.getElementById('relative-circle-animation-container')
-const allCardsContainer = document.getElementsByClassName('all-generated-cards-container')[0];
 
 burgerMenu.addEventListener("click", function () {
   burgerMenu.classList.toggle("burger-active")
 });
-
-async function pulseAnimationHandler() {
-  const delay = ms => new Promise(res => setTimeout(res, ms));
-  await delay(2500);
-  pulse.classList.toggle("make-disappear");
-  allCardsContainer.classList.toggle("make-disappear");
-}
-
-const init = async () => {
-  await pulseAnimationHandler();
-}
-
-window.onload = init;
-
-/*-------------Gestion des cartes de recettes-------------*/
-
