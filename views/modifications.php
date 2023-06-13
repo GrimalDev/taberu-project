e2971cb95575f04376c1e95ff70dc1f1a0255d54
Grimaldi Baptiste <79135575+GrimalDev@users.noreply.github.com>
2023-06-13 09:51:04 +0200

    Regex correction on password verification

--- views/modifications.php
+++ views/modifications.php
@@ -36,7 +36,9 @@ function verifyFormData() {
 
     if(!$user->verifyPassword($password_initial)) { return "Mauvais mot de passe"; }
 
-    if (!preg_match("/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,32}$/", FORM_PASSWORD)) {
+    $passwordPattern = '/^(?=.*[@!%*?&])(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,32}$/';
+
+    if (!preg_match($passwordPattern, FORM_PASSWORD)) {
         return "Mot de passe incorrect,<br />&nbsp- Minimum un caractère spécial (@!%*?&),<br />&nbsp- Minimum une Majuscule ainsi que une minuscule,<br />&nbsp- Entre 8 et 32 caractères.";
     }; //Password has been verified
 
